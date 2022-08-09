549c018f7a26b4f52374ba8a392f6e0ff01c7c5a
Mihir Chaturvedi <plibither8@gmail.com>
2022-08-09 17:54:47 +0530

    refactor: New worker syntax

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
+import { Router } from "itty-router";
+
+const router = Router();
+
 const base = "https://mihir.ch";
 const statusCode = 301;
 
-async function handleRequest(request: Request): Promise<Response> {
+router.all("*", (request) => {
   const url = new URL(request.url);
   const { pathname, search, host } = url;
   const destinationURL = `${base}${pathname}${search}${
     search ? "&" : "?"
   }from=${host}`;
   return Response.redirect(destinationURL, statusCode);
-}
-
-addEventListener("fetch", async (event) => {
-  event.respondWith(handleRequest(event.request));
 });
+
+export default {
+  async fetch(request: Request): Promise<Response> {
+    return router.handle(request);
+  },
+};
